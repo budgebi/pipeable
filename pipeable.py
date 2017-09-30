28eb4cbeeb39ee2765b74871c5b43b2ec5cfe646
budgebi <budgebrian21@gmail.com>
2017-09-30 16:35:18 -0400

    renamed pipeable to pipe. Made get input private or at least suggested it should be

--- pipeable.py
+++ pipeable.py
@@ -2,7 +2,10 @@ import sys
 
 used = False
 
-def get_input():
+def __get_input():
+    """ Gets the input from stdin if it exists.
+        Otherwise, it looks for a command line argument.
+        Raises an error if no argument is given. """
     if not sys.stdin.isatty():
         inp = sys.stdin.read()
     else:
@@ -13,19 +16,21 @@ def get_input():
             raise IndexError(message)
     return inp
 
-def pipeable(transform=None):
-    def real_pipeable(func):
+def pipe(transform=None):
+    """ Decorator to read the input from stdin or as a command line argument
+        and pass the result to the decorated function. """
+    def pipe_in(func):
         global used
         if used:
-            raise Exception('Error: cannot decorate multiple functions with @pipeable!')
+            raise Exception('Error: cannot decorate multiple functions with @pipe!')
 
         used = True
         def wrapper_func():
-            inp = get_input()
+            inp = __get_input()
             if transform != None:
                 func(transform(inp))
             else:
                 func(inp)
 
         wrapper_func()
-    return real_pipeable
+    return pipe_in
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='pipeable',
-      version='1.0',
+      version='0.0.0',
       description='A utility for adding python scripts into pipelines',
       author='Brian Budge',
       author_email='budgebrian21@gmail.com',
-      url='',
-      py_modules=['pipeable'],
+      url='https://github.com/budgebi/pipeable',
+      py_modules=['pipeable']
      )
