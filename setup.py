7913d3595ba3f21a08bec48b879e11b18750b27c
budgebi <budgebrian21@gmail.com>
2017-10-01 12:41:30 -0400

    Add pertinent information to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,21 @@ from distutils.core import setup
 
 setup(name='pipeable',
       version='0.0.0',
-      description='A utility for adding python scripts into pipelines',
+      description='A utility for making Python scripts compatible with the | operator on the command line.',
       author='Brian Budge',
       author_email='budgebrian21@gmail.com',
       url='https://github.com/budgebi/pipeable',
-      py_modules=['pipeable']
+      license='MIT',
+      py_modules=['pipeable'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+      ],
+      keywords='pipeline, pipe, pipeable',
+      install_require=[],
+      extra_require=[]
      )
