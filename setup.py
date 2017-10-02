61d6e0cc6d0040cb827ef2ed3103a68791ec1f91
budgebi <budgebrian21@gmail.com>
2017-10-02 17:16:31 -0400

    Change to version 1.0.1

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='pipeable',
-      version='1.0.0',
+      version='1.0.1',
       description='A utility for making Python scripts compatible with the | operator on the command line.',
       author='Brian Budge',
       author_email='budgebrian21@gmail.com',
       url='https://github.com/budgebi/pipeable',
       license='MIT',
       packages=find_packages(exclude=('tests')),
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-      ],
+      python_requires='>=3',
       keywords='pipeline, pipe, pipeable',
       install_require=[],
       extra_require=[]
